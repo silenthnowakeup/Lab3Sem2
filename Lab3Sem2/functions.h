b1c9de0c0f87e0c6b77745ca7fce2b997b3095e8
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2023-04-11 22:30:20 +0300

    Update functions.h

--- Lab3Sem2/functions.h
+++ Lab3Sem2/functions.h
@@ -5,51 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "struct.h"
 
 #ifndef LAB3SEM2_FUNCTIONS_H
 #define LAB3SEM2_FUNCTIONS_H
-
-#pragma pack(1)
-
-typedef struct BMPHeader
-{
-    unsigned char ID[2];
-    unsigned int fileSize;
-    unsigned char unused[4];
-    unsigned int pixelOffset;
-}BMPHeader;
-
-typedef struct DIBHeader
-{
-    unsigned int headerSize;
-    unsigned int width;
-    unsigned int height;
-    unsigned short colorPlanes;
-    unsigned short bitsPerPixel;
-    unsigned int comp;
-    unsigned int dataSize;
-    unsigned int pwidth;
-    unsigned int pheight;
-    unsigned int colorsCount;
-    unsigned int ipmColorsCount;
-}DIBHeader;
-
-typedef struct Pixel {
-    unsigned char red;
-    unsigned char green;
-    unsigned char blue;
-} Pixel;
-
-typedef struct BMPFile
-{
-    BMPHeader bmphdr;
-    DIBHeader dibhdr;
-    unsigned char colorTable[1024];
-    Pixel** pixels;
-}BMPFile;
-
-#pragma pack(pop)
-
 BMPFile* loadBMPFile(const char* fileName);
 void freeBMPFile(BMPFile* bmp_file);
 char* inputStr();
